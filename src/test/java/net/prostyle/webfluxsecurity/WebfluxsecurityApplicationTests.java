ccec8c3a3708ddf619ff8237ffd26b2d89aee7da
006ADD <zhakypbekov.adilet@bk.ru>
2024-04-28 14:29:25 +0600

    Initializer

---
+++ src/test/java/net/prostyle/webfluxsecurity/WebfluxsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package net.prostyle.webfluxsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
